b3293861f1efe7275c9cc6d3ed06c3b223702ef5
priyanka-sharma181 <priyanka.sharma@madgicaltechdom.com>
2025-08-21 23:21:08 +0530

    Stop tracking dist directory

--- dist/tasks/dto/create-task.dto.d.ts
+++
@@ -1,7 +0,0 @@
-import { TaskStatus } from '../entities/task.entity';
-export declare class CreateTaskDto {
-    title: string;
-    description?: string;
-    status?: TaskStatus;
-    dueDate?: Date;
-}
--- dist/users/entities/user.entity.d.ts
+++
@@ -1,9 +0,0 @@
-import { Task } from '../../tasks/entities/task.entity';
-export declare class User {
-    id: number;
-    email: string;
-    password: string;
-    createdAt: Date;
-    updatedAt: Date;
-    tasks: Task[];
-}
